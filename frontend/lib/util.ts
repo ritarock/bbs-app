0fb9b07d01b6caae29c01ca0ec1a18c5a4fce03d
ritarock <dvryz1204@gmail.com>
2023-01-18 10:01:25 +0900

    init

--- frontend/lib/util.ts
+++
@@ -1,7 +0,0 @@
-export const toDateFormat = (t: Date) => {
-  const dt = new Date(t);
-
-  return dt.getFullYear().toString() + "-" + ("0" + dt.getMonth()).slice(-2) +
-    "-" + ("0" + dt.getDate()).slice(-2) + " " +
-    ("0" + dt.getHours()).slice(-2) + ":" + ("0" + dt.getMinutes()).slice(-2);
-};
